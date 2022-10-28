b8dae30e62056ad729992bd89c0158a2717def07
Ariane <arianerochaf@gmail.com>
2022-10-28 07:58:17 -0300

    update

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    // pegará os parametros na rota
+    this.activatedRoute.params.subscribe(
+      res => console.log(res['id'], res['username'], res)
+    )
+    this.activatedRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+    setInterval( () => {
+     this.router.navigate(['404']); // indicado para navegações internas
+     // this.router.navigateByUrl('404'); // indicado para navegações externa, pois ocorre o reaload da página
+    }, 3000)
   }
 
 }
